b5bfffb41a2fab56853f3f95949b03d50d54a2dc
Kompot <69313786+kompot69@users.noreply.github.com>
2022-12-05 22:01:54 +0300

    Update FTG_FileDownloader.py

--- FTG_FileDownloader.py
+++ FTG_FileDownloader.py
@@ -38,8 +38,9 @@ class DownloadMod(loader.Module):
 
         filename=str(args[0])
         while "/" in filename:
-           filename=str(filename.partition('/')[2]
-        command = "wget -O",filename,str(args[0]))
+           filename=filename.partition('/')[2]
+        command = "wget -O",filename,str(args[0])
+           filename=str(filename)
 
         await message.edit("<b>Загружаю файл "+filename+"...</b>") 
         try:
