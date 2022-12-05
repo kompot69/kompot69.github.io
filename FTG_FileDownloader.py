3cb35879d6bc8125d7f63eb1df71e2d00aa15d36
Kompot <69313786+kompot69@users.noreply.github.com>
2022-12-05 20:24:10 +0300

    Update FTG_FileDownloader.py

--- FTG_FileDownloader.py
+++ FTG_FileDownloader.py
@@ -45,7 +45,7 @@ class DownloadMod(loader.Module):
 
         await message.edit("<b>Выгружаю файл...</b>")
         try:
-           await client.send_file(message.to_id, tempfile)
+           await message.client.send_file(message.to_id, tempfile)
         except ValueError:
            await message.edit("<b>Не удалось выгрузить файл!</b>")
 
